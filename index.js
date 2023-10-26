ccf2a23feb346b0340117fed1bf5e7f2712ca143
Enocho1-1 <enoch.okyere@gmail.com>
2023-10-26 12:23:59 -0400

    cinemaUni backend

---
+++ index.js
@@ -0,0 +1,27 @@
+import  express  from "express";
+import jsonServer from "json-server";
+import auth from "json-server-auth";
+
+const server = express();
+server.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
+
+const router = jsonServer.router('./Data/db.json');
+server.use('/api', router);
+server.db = router.db
+
+const middlewares = jsonServer.defaults()
+const rules = auth.rewriter({
+    orders:660,
+    users:600
+})
+
+server.use(rules)
+server.use(auth)
+server.use(middlewares)
+server.use(router)
+
+server.listen(15000)
